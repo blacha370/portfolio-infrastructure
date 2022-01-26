f3ff19a0df8348826460ffba94617917ce9ff445
blacha370 <lukasz.blacha370@gmail.com>
2022-01-26 21:07:03 +0100

    Create bucket for frontend files

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
-	backend "s3" {
-		bucket = "apps-terraform-config"
-		key    = "portfolio/terraform.tfstate"
-		region = "eu-central-1"
-	}
+  backend "s3" {
+    bucket = "apps-terraform-config"
+    key    = "portfolio/terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
